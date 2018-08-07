eade332fd151271724b5ea3ef83d50d5e66b427f
tianpengfei <tpf1220@sina.com>
2018-08-07 17:58:12 +0800

    change commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        Hello react
         
       </div>
     );
